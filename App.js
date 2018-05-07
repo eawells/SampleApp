f111c488bf4ebaf08771821ae90c141619ae9110
Elizabeth Wells <elizabeth.a.wells1@gmail.com>
2018-05-07 16:50:17 -0500

    Flex box maddness

--- App.js
+++ App.js
@@ -6,26 +6,54 @@ import {
   View
 } from 'react-native';
 
-const styles = StyleSheet.create({
-  defaultText: {
-    fontSize: 22,
-    padding: 10,
-    margin: 5,
-    borderWidth: 1,
-    color: 'black'
-  }
-})
-
 export default class App extends React.Component {
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <StatusBar hidden={true} />
-        <Text style={styles.defaultText}>Cool dude</Text>
-        <Text style={styles.defaultText}>Happy dude</Text>
-        <Text style={styles.defaultText}>Sad dude</Text>
+        <Text style={[styles.defaultText,styles.coolText]}>Cool dude</Text>
+        <Text style={[styles.defaultText,styles.happyText]}>Happy dude</Text>
+        <Text style={[styles.defaultText,styles.sadText]}>Sad dude</Text>
+        <View style={styles.container}>
+        <Text style={[styles.defaultText, styles.image]}>[Insert Image]</Text>
+        </View>
       </View>
     )
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    justifyContent: 'space-around',
+    backgroundColor: 'blueviolet'
+  },
+  defaultText: {
+    textAlign: 'center',
+    fontSize: 20,
+    padding: 10,
+    margin: 5,
+    borderWidth: StyleSheet.hairlineWidth,
+    color: 'black'
+  },
+  happyText: {
+    alignSelf: 'flex-end',
+    backgroundColor: 'yellow',
+    color: 'black',
+    fontWeight: 'bold'
+  },
+  coolText: {
+    backgroundColor: 'cyan',
+    color: 'blue'
+  },
+  sadText: {
+    backgroundColor: 'black',
+    color: 'cyan'
+  },
+  image: {
+    flexDirection: 'row',
+    backgroundColor: 'white',
+  }
+})
